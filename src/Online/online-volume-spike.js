8a1ec19eefb2eb09bf16e5b93696564f56dc71e8
festodidactic-cell <festo.didactic@gmail.com>
2026-09-23 02:12:53 +0330

    Create online-volume-spike.js

---
+++ src/Online/online-volume-spike.js
@@ -0,0 +1,20 @@
+true==function(){
+
+var todayVolume=(qd1);
+
+var averageVolume=
+(bvol+0);
+
+
+if(
+todayVolume>(averageVolume*2) &&
+(pl)>0 &&
+(pd1)>0
+)
+
+return true;
+
+
+return false;
+
+}()

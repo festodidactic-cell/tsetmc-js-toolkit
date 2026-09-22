bc1cfe1ef3ffdf4e7cb3cec4d6aa002abee2f1c0
festodidactic-cell <festo.didactic@gmail.com>
2026-09-23 02:14:23 +0330

    Create online-tight-range-break.js

---
+++ src/Online/online-tight-range-break.js
@@ -0,0 +1,25 @@
+true==function(){
+
+
+var spread=
+((pl)-(pf));
+
+
+var percent=
+(spread/(pf))*100;
+
+
+if(
+percent>1 &&
+percent<5 &&
+(qd1)>((bvol)*0.5) &&
+(pl)>0
+)
+
+return true;
+
+
+return false;
+
+
+}()
